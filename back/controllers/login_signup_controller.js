7b8a799c4f051b2e386a9a6aad84b04cfbb7f9a0
Reza-Kh1 <r.khani1385.66@gmail.com>
2024-03-13 19:56:23 +0330

    ok

--- back/controllers/login_signup_controller.js
+++ back/controllers/login_signup_controller.js
@@ -1,203 +1,199 @@
-import {userModel} from '../models/index.js';
+import { userModel } from '../models/index.js';
 import { errorHandler } from '../middlewares/errorHandler.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import asyncHandler from "express-async-handler"
 import { Op } from 'sequelize';
 
-const register = asyncHandler(async(req, res, next) => {
-    const {phoneNumber, type, password, userName, melliNumber} = req.body;
-    let newUser
-    let hashedPwd
+const register = asyncHandler(async (req, res, next) => {
+  let { phoneNumber, type, password, userName, melliNumber } = req.body;
+  let newUser
+  let hashedPwd
 
-    try {
-      
-      if( type === 0){
-        
-        hashedPwd = await bcrypt.hash(password, 10)
-        newUser = await userModel.create({phoneNumber, type, password:hashedPwd, companyNumber:userName}, { exclude: ["password"] });
-      }
-      else {
-        hashedPwd = await bcrypt.hash(password || "1234", 10)
-        newUser = await userModel.create({phoneNumber, type, password:hashedPwd, melliNumber}, { exclude: ["password"] });
-      }
-        
-        
-
-        const token = jwt.sign({
-            user:{
-                id: newUser.id,
-                type: newUser.type,
-                phoneNumber: newUser.phoneNumber
-            }
-        }, "this is my secret key", {
-            expiresIn: "24h"
-          });
-    
-          res.send({
-            token: token,
-            userName: newUser.userName,
-            melliNumber: newUser.melliNumber,
-            phoneNumber: newUser.phoneNumber,
-            type: newUser.type,
-            companyNumber: newUser.companyNumber,
-            id: newUser.id
-
-          });
-    } catch (error) {
-        console.log(error)
-        return next(errorHandler(500, error))
-        
+  try {
+    const data = await userModel.count()
+    if (!data) {
+      type = 1
     }
+    hashedPwd = await bcrypt.hash(password, 10)
+    newUser = await userModel.create({ phoneNumber, type: type || 0, password: hashedPwd, companyNumber: userName }, { exclude: ["password"] });
+
+
+    const token = jwt.sign({
+      user: {
+        id: newUser.id,
+        type: newUser.type,
+        phoneNumber: newUser.phoneNumber
+      }
+    }, "this is my secret key", {
+      expiresIn: "24h"
+    });
+
+    res.send({
+      token: token,
+      userName: newUser.userName,
+      melliNumber: newUser.melliNumber,
+      phoneNumber: newUser.phoneNumber,
+      type: newUser.type,
+      companyNumber: newUser.companyNumber,
+      id: newUser.id
+
+    });
+  } catch (error) {
+    console.log(error)
+    return next(errorHandler(500, error))
+
+  }
 })
 
-const login = asyncHandler(async(req, res, next) => {
-    const {phoneNumber, password} = req.body
-    console.log(req.body)
+const login = asyncHandler(async (req, res, next) => {
+  const { phoneNumber, password } = req.body
+  console.log(req.body)
 
-    if(!phoneNumber || !password){
-        return next(errorHandler(400, "All fields are required"))
-    }
-    
-    try {
-        const user = await userModel.findOne({
-            where: {
-              phoneNumber,
-            },
+  if (!phoneNumber || !password) {
+    return next(errorHandler(400, "All fields are required"))
+  }
 
-        });
-        console.log(user)
-        if (!user) {
-            return next(errorHandler(400, "Wrong credentials!"))
-        }
-       
-        const matched = await bcrypt.compare(
-            req.body.password,
-            user.dataValues.password
-        );
-  
-        if (!matched) {
-            return next(errorHandler(400, "Wrong credentials!"));
-        }
-
-        const token = jwt.sign({
-            user:{
-                id: user.id,
-                type: user.type,
-                phoneNumber: user.phoneNumber
-            }
-        }, "this is my secret key", {
-            expiresIn: "24h"
-        });
-        
-      
-        res.send({
-            token: token,
-            userName: user.userName,
-            melliNumber: user.melliNumber,
-            phoneNumber: user.phoneNumber,
-            type: user.type,
-            companyNumber: user.companyNumber,
-            id: user.id
-
-          });
-        
-    } catch (error) {
-        console.log(error)
-        next(errorHandler(500, error))
-        
-    }
-
-
-})
-
-const getAllusers = asyncHandler(async(req, res, next)=>{
-    try {
-        const users = await userModel.findAll({
-      });
-        res.send(users);
-      } catch (error) {
-        console.log(error)
-        next(errorHandler(500, error))
-      }
-})
-
-const changePassword = asyncHandler(async(req, res, next)=>{
-    let password = req.body.currentPassword;
+  try {
     const user = await userModel.findOne({
       where: {
-        id: req.params.id,
+        phoneNumber,
       },
-    });
-    try {
-      const hash = await bcrypt.hash(req.body.newPassword, 10);
-      const matched = await bcrypt.compare(password, user.dataValues.password);
 
-      if (!matched) {
-        return res.send({ success: false });
-      }
-      // const updatedUser = await Users.updateOne({ email }).set({
-      //   password: hash
-      // })
-      const updatedUser = await userModel.update(
-        { password: hash },
-        {
-          where: {
-            id: req.params.id,
-          },
-        }
-      );
-      if (!updatedUser) {
-        return next(errorHandler(500, "server error"))
-      }
-      return res.send({ success: true });
-    } catch (error) {
-      // sails.error(e);
-      return next(errorHandler(500, error))
+    });
+    console.log(user)
+    if (!user) {
+      return next(errorHandler(400, "Wrong credentials!"))
     }
+
+    const matched = await bcrypt.compare(
+      req.body.password,
+      user.dataValues.password
+    );
+
+    if (!matched) {
+      return next(errorHandler(400, "Wrong credentials!"));
+    }
+
+    const token = jwt.sign({
+      user: {
+        id: user.id,
+        type: user.type,
+        phoneNumber: user.phoneNumber
+      }
+    }, "this is my secret key", {
+      expiresIn: "24h"
+    });
+
+
+    res.send({
+      token: token,
+      userName: user.userName,
+      melliNumber: user.melliNumber,
+      phoneNumber: user.phoneNumber,
+      type: user.type,
+      companyNumber: user.companyNumber,
+      id: user.id
+
+    });
+
+  } catch (error) {
+    console.log(error)
+    next(errorHandler(500, error))
+
+  }
+
+
 })
 
-const findUsers = asyncHandler(async(req, res, next)=>{
-  let {text, skip, take} = req.query
+const getAllusers = asyncHandler(async (req, res, next) => {
+  try {
+    const users = await userModel.findAll({
+    });
+    res.send(users);
+  } catch (error) {
+    console.log(error)
+    next(errorHandler(500, error))
+  }
+})
+
+const changePassword = asyncHandler(async (req, res, next) => {
+  let password = req.body.currentPassword;
+  const user = await userModel.findOne({
+    where: {
+      id: req.params.id,
+    },
+  });
+  try {
+    const hash = await bcrypt.hash(req.body.newPassword, 10);
+    const matched = await bcrypt.compare(password, user.dataValues.password);
+
+    if (!matched) {
+      return res.send({ success: false });
+    }
+    // const updatedUser = await Users.updateOne({ email }).set({
+    //   password: hash
+    // })
+    const updatedUser = await userModel.update(
+      { password: hash },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!updatedUser) {
+      return next(errorHandler(500, "server error"))
+    }
+    return res.send({ success: true });
+  } catch (error) {
+    // sails.error(e);
+    return next(errorHandler(500, error))
+  }
+})
+
+const findUsers = asyncHandler(async (req, res, next) => {
+  let { text, skip, take } = req.query
   text = text || ""
   skip = skip || 1
   take = take || 10
 
-  
-  
+
+
 
   try {
     const users = await userModel.findAndCountAll({
-      where:{
-        [Op.or]:[{
-          nameAndFamily: {[Op.substring]: text}
+      where: {
+        [Op.or]: [{
+          nameAndFamily: { [Op.substring]: text }
         },
         {
-          phoneNumber: {[Op.substring]: text}
+          phoneNumber: { [Op.substring]: text }
         },
         {
-          melliNumber: {[Op.substring]: text}
+          melliNumber: { [Op.substring]: text }
         },
         {
-          companyNumber: {[Op.substring]: text}
+          companyNumber: { [Op.substring]: text }
         }]
       },
       offset: take * (skip - 1), // Calculating offset
       limit: take,
     })
-    
-    res.send({users: users.rows, totalPages: Math.ceil(users.count / take)})
+
+    res.send({ users: users.rows, totalPages: Math.ceil(users.count / take) })
   } catch (error) {
     console.log(error)
-    return next(errorHandler(500,error))
-    
+    return next(errorHandler(500, error))
+
   }
 })
 
-const sendToEval = asyncHandler(async(req,res,next)=>{
-  try {console.log("hi")
+const sendToEval = asyncHandler(async (req, res, next) => {
+  try {
+    console.log("hi")
     const user = await userModel.findOne({
-      where: {companyId: req.params.id}
+      where: { companyId: req.params.id }
     })
 
     const companyDetailsComplete = user.companyDetailsComplete || false;
@@ -207,7 +203,7 @@ const sendToEval = asyncHandler(async(req,res,next)=>{
     const employeesComplete = user.employeesComplete || false
     let sendFlag = true
 
-    if(!companyDetailsComplete || !salesComplete || !knowledgeSalesComplete || !exportsComplete || !employeesComplete){
+    if (!companyDetailsComplete || !salesComplete || !knowledgeSalesComplete || !exportsComplete || !employeesComplete) {
       sendFlag = false
     }
 
@@ -218,23 +214,23 @@ const sendToEval = asyncHandler(async(req,res,next)=>{
       knowledgeSalesComplete,
       exportsComplete,
       employeesComplete
-      
+
     }, {
-      where: {companyId: req.params.id},
+      where: { companyId: req.params.id },
       returning: true
-    }) 
+    })
 
     req.user = updatedUser[0].dataValues
 
     res.send({
-      sendToEval: req.user.sendToEval ,
+      sendToEval: req.user.sendToEval,
       companyDetailsComplete: req.user.companyDetailsComplete,
       salesComplete: req.user.salesComplete,
       knowledgeSalesComplete: req.user.knowledgeSalesComplete,
       exportsComplete: req.user.exportsComplete,
       employeesComplete: req.user.employeesComplete
     })
-    
+
   } catch (error) {
     console.log(error)
     return next(errorHandler(500, error))
@@ -242,10 +238,10 @@ const sendToEval = asyncHandler(async(req,res,next)=>{
 })
 
 export {
-    register,
-    login,
-    getAllusers,
-    changePassword,
-    findUsers,
-    sendToEval
+  register,
+  login,
+  getAllusers,
+  changePassword,
+  findUsers,
+  sendToEval
 }
